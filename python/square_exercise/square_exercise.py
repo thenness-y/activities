49e7f51fe885cadc687d2124b953a2d5ff82f504
thenness-y <vhennessy@ucsd.edu>
2025-01-28 14:39:55 -0800

    Update square_exercise.py
    question_10

--- python/square_exercise/square_exercise.py
+++ python/square_exercise/square_exercise.py
@@ -3,10 +3,28 @@ import sys
 from psychopy import visual,event,core # import the bits of PsychoPy we'll need for this exercise
 
 win = visual.Window([400,400],color="black", units='pix',checkTiming=False) #open a window
-square = visual.Rect(win,lineColor="black",fillColor="red",size=[100,100]) #create a Rectangle type object with certain parameters
-square.draw() #draw the square to the screen buffer
-win.flip() #make the buffer visible, i.e., show what's been drawn to it
-core.wait(1.5) #pause for half a second (i.e., 500 ms)
+left_square = visual.Rect(win,lineColor="black",fillColor="green",size=[100,100],pos = (-100,0))
+right_square = visual.Rect(win,lineColor="black",fillColor="pink",size=[100,100],pos = (100,0))
+
+# color_trials = ["blue", "red"]*3
+
+spinning = True
+while True:
+    if spinning:
+        left_square.ori +=6
+        left_square.draw()
+        right_square.ori +=-6
+        right_square.draw()
+        win.flip()
+
+    if event.getKeys('s'):
+        spinning=False
+
+    if event.getKeys('r'):
+        spinning=True
+
+    if event.getKeys('q'):
+       break   
+
 win.close() #close the window -- don't need this if you're running this as a separate file
 core.quit() #quit out of the program -- don't need this if you're running this as a separate file
-
